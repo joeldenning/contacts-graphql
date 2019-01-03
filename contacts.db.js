c48d1b7e36a30ce8a89e58d66e4c864de1627e0c
Joel Denning <joeldenning@gmail.com>
2019-01-02 18:16:51 -0700

    Apollo

--- contacts.db.js
+++ contacts.db.js
@@ -3,11 +3,78 @@ let nextId = 10
 const contacts = [
   {
     id: '1',
+    isBusiness: false,
     name: "Johnny Appleseed",
+    contactType: "client",
+    phones: {
+      homePhone: {
+        number: '8014444444',
+      },
+    },
+    emailAddresses: {
+      personalEmail: 'why@hello.com',
+    },
+    occupation: "Software Engineer",
+    addresses: {
+      mailingAddress: {
+        isPrimary: false,
+        addressLine1: '2524 E 3444 S',
+        addressLine2: null,
+        country: 'United States',
+        locality: 'Salt Lake City',
+        postalCode: '84199',
+        region: 'UT'
+      },
+      homeAddress: {
+        isPrimary: true,
+        addressLine1: '2524 E 3444 S',
+        addressLine2: null,
+        country: 'United States',
+        locality: 'Salt Lake City',
+        postalCode: '84199',
+        region: 'UT'
+      },
+      workAddress: {
+        isPrimary: false,
+        addressLine1: '2524 E 3444 S',
+        addressLine2: null,
+        country: 'United States',
+        locality: 'Salt Lake City',
+        postalCode: '84199',
+        region: 'UT'
+      },
+      otherAddress: {
+        isPrimary: false,
+        addressLine1: '2524 E 3444 S',
+        addressLine2: null,
+        country: 'United States',
+        locality: 'Salt Lake City',
+        postalCode: '84199',
+        region: 'UT'
+      },
+    },
+    ssn: '234234243',
+    dateOfBirth: 123,
+    occupation: 'Engineer',
+    clientSince: 123,
+    createdAt: 123,
+    source: 'referral',
+    additionalInfo: 'This person is sorta hard to work with.',
   },
   {
     id: '2',
-    name: "Kurt Avarell",
+    isBusiness: true,
+    name: "Jackson Hewitt",
+    contactType: "prospect",
+    addresses: {
+      mailingAddress: {
+        isPrimary: false,
+      },
+    },
+    contactPerson: "Rod Gilbert",
+    additionalInfo: "Enterprisey",
+    industry: 'GOVERNMENT',
+    businessType: 'C_CORPORATION',
   },
 ]
 
@@ -18,19 +85,34 @@ exports.getAllContacts = () => {
   }
 }
 
-exports.createContact = input => {
-  const fullContact = {...input.contact, id: nextId++}
+exports.createIndividualContact = (_, input) => {
+  const contact = input.newContact
+  contact.isBusiness = false
+  contact.name = contact.firstName + (contact.middleInitial ? ` ${contact.middleInitial}` : '') + (contact.lastName ? ` ${contact.lastName}` : '')
+  contact.addresses = contact.addresses || {}
+  contact.emailAddresses = contact.emailAddresses || {}
+  const fullContact = {...contact, id: String(nextId++)}
   contacts.push(fullContact)
 
   return fullContact
 }
 
-exports.updateContact = input => {
-  console.log('input', input)
+exports.createBusinessContact = (_, input) => {
+  const contact = input.newContact
+  contact.isBusiness = true
+  contact.addresses = contact.addresses || {}
+  const fullContact = {...contact, id: String(nextId++)}
+  contacts.push(fullContact)
+
+  return fullContact
+}
+
+exports.updateContact = (_, input) => {
   const newContact = input.contact
-  const existingContact = contacts.find(contact => contact.id === newContact.id)
+  const id = input.contactId
+  const existingContact = contacts.find(contact => contact.id === id)
   if (!existingContact) {
-    throw Error(`Cannot update contact with id '${newContact.id}' -- no such contact`)
+    throw Error(`Cannot update contact with id '${id}' -- no such contact`)
   }
 
   const result = Object.assign(existingContact, newContact)
--- index.js
+++ index.js
@@ -1,49 +1,295 @@
-const express = require('express');
-const express_graphql = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require('express')
+const {ApolloServer, gql} = require('apollo-server-express')
 const db = require('./contacts.db.js')
 
 // GraphQL schema
-const schema = buildSchema(`
+const typeDefs = gql(`
   type Query {
     allContacts: ContactList!
   }
 
   type Mutation {
-    createContact(contact: CreateContactInput): FullContact!
-    updateContact(contact: UpdateContactInput): FullContact!
+    createIndividualContact(newContact: IndividualContactInput): IndividualContact
+    updateIndividualContact(contactId: ID, contact: UpdateIndividualContactInput): IndividualContact
+    createBusinessContact(newContact: BusinessContactInput): BusinessContact
+    updateBusinessContact(contactId: ID, contact: UpdateBusinessContactInput): BusinessContact
+  }
+
+  input BusinessContactInput {
+    contactType: ContactType!
+    name: String!
+    addresses: BusinessAddressesInput
+    contactPerson: String
+    ein: String
+    businessType: BusinessType
+    dateEstablished: Int
+    industry: BusinessIndustry
+    clientSince: Int
+    source: String
+    additionalInfo: String
+  }
+
+  input UpdateBusinessContactInput {
+    contactType: ContactType
+    name: String
+    addresses: BusinessAddressesInput
+    contactPerson: String
+    ein: String
+    businessType: BusinessType
+    dateEstablished: Int
+    industry: BusinessIndustry
+    clientSince: Int
+    source: String
+    additionalInfo: String
+  }
+
+  input UpdateIndividualContactInput {
+    contactType: ContactType
+    firstName: String
+    middleName: String
+    lastName: String
+    addresses: IndividualAddressesInput
+    emails: IndividualContactEmailsInput
+    ssn: String
+    dateOfBirth: Int
+    occupation: String
+    clientSince: Int
+    createdAt: Int
+    source: String
+    additionalInfo: String
+  }
+
+  input BusinessAddressesInput {
+    mailingAddress: AddressInput
+    physicalAddress: AddressInput
+    otherAddress: AddressInput
+  }
+
+  input IndividualContactInput {
+    contactType: ContactType!
+    firstName: String!
+    middleInitial: String
+    lastName: String
+    addresses: IndividualAddressesInput
+    emails: IndividualContactEmailsInput
+    ssn: String
+    dateOfBirth: Int
+    occupation: String
+    clientSince: Int
+    createdAt: Int
+    source: String
+    additionalInfo: String
+  }
+
+  input IndividualAddressesInput {
+    mailingAddress: AddressInput
+    homeAddress: AddressInput
+    workAddress: AddressInput
+    otherAddress: AddressInput
+  }
+
+  input IndividualContactEmailsInput {
+    personalEmail: String
+    workEmail: String
+    otherEmail: String
+  }
+
+  input AddressInput {
+    isPrimary: Boolean!
+    addressLine1: String
+    addressLine2: String
+    country: String
+    locality: String
+    postalCode: String
+    region: String
   }
 
   type ContactList {
     totalCount: Int!
-    contacts: [FullContact!]!
+    contacts: [Contact!]!
   }
 
-  type FullContact {
+  interface Contact {
     id: ID!
+    isBusiness: Boolean!
+    contactType: ContactType!
     name: String!
+    addresses: Addresses!
+    additionalInfo: String
   }
 
-  input CreateContactInput {
-    name: String!
+  enum ContactType {
+    client
+    prospect
+    other
   }
 
-  input UpdateContactInput {
+  interface Addresses {
+    mailingAddress: Address
+  }
+
+  type Address {
+    isPrimary: Boolean!
+    addressLine1: String
+    addressLine2: String
+    country: String
+    locality: String
+    postalCode: String
+    region: String
+  }
+
+  type IndividualContact implements Contact {
     id: ID!
+    isBusiness: Boolean!
+    contactType: ContactType!
     name: String!
+    firstName: String!
+    middleInitial: String
+    lastName: String!
+    phones: IndividualContactPhones!
+    emailAddresses: IndividualContactEmails!
+    addresses: IndividualAddresses!
+    ssn: String
+    dateOfBirth: Int
+    occupation: String
+    clientSince: Int
+    createdAt: Int
+    source: String
+    additionalInfo: String
+  }
+
+  type IndividualAddresses implements Addresses {
+    mailingAddress: Address
+    homeAddress: Address
+    workAddress: Address
+    otherAddress: Address
+  }
+
+  type IndividualContactPhones {
+    homePhone: Phone
+    mobilePhone: Phone
+    workPhone: Phone
+    faxNumber: Phone
+    otherPhone: Phone
+  }
+
+  type IndividualContactEmails {
+    personalEmail: String
+    workEmail: String
+    otherEmail: String
+  }
+
+  type Phone {
+    number: String!
+    extension: String
+  }
+
+  type BusinessContact implements Contact {
+    id: ID!
+    isBusiness: Boolean!
+    contactType: ContactType!
+    name: String!
+    addresses: BusinessAddresses!
+    contactPerson: String
+    ein: String
+    businessType: BusinessType
+    dateEstablished: Int
+    industry: BusinessIndustry
+    clientSince: Int
+    createdOn: Int
+    source: String
+    additionalInfo: String
+  }
+
+  type BusinessAddresses implements Addresses {
+    mailingAddress: Address
+    physicalAddress: Address
+    otherAddress: Address
+  }
+
+  enum BusinessType {
+    C_CORPORATION
+    S_CORPORATION
+    LLC
+    PARTNERSHIP
+    SOLE_PROPRIETORSHIP
+    ESTATE
+    TRUST
+    NON_PROFIT
+    OTHER
+  }
+
+  enum BusinessIndustry {
+    ADMINISTRATIVE_AND_SUPPORT
+    AGRICULTURE
+    APPAREL
+    ARTS
+    CHEMICALS
+    CONSTRUCTION
+    EDUCATION
+    ELECTRONICS
+    ENERGY
+    ENGINEERING
+    ENTERTAINMENT
+    FINANCE
+    FISHING
+    FOOD_SERVICES
+    FORESTRY
+    GOVERNMENT
+    HEALTH_CARE
+    HOSPITALITY
+    HUNTING
+    INFORMATION_TECHNOLOGY
+    INSURANCE
+    MANAGEMENT
+    MANUFACTURING
+    MEDIA
+    MINING
+    OTHER
+    PHARMACEUTICALS
+    PROFESSIONAL_SERVICES
+    REAL_ESTATE
+    RECREATION
+    REMEDIATION_SERVICES
+    RENTAL_AND_LEASING
+    RETAIL_TRADE
+    SCIENTIFIC_SERVICES
+    SHIPPING
+    SOCIAL_ASSISTANCE
+    TECHNICAL_SERVICES
+    TRANSPORTATION
+    UTILITIES
+    WAREHOUSING
+    WASTE_MANAGEMENT
+    WHOLESALE_TRADE
   }
 `);
+
 // Root resolver
-const root = {
-  allContacts: db.getAllContacts,
-  createContact: db.createContact,
-  updateContact: db.updateContact,
+const resolvers = {
+  Query: {
+    allContacts: db.getAllContacts,
+  },
+  Mutation: {
+    createIndividualContact: db.createIndividualContact,
+    updateIndividualContact: db.updateContact,
+    createBusinessContact: db.createBusinessContact,
+    updateBusinessContact: db.updateContact,
+  },
+  Contact: {
+    contactType: obj => obj.contactType,
+    __resolveType: obj => obj.isBusiness ? 'BusinessContact' : 'IndividualContact',
+  },
+  Addresses: {
+    __resolveType: (...args) => {
+      console.log('resolving', ...args)
+      return 'IndividualAddresses'
+    }
+  },
 };
+
 // Create an express server and a GraphQL endpoint
 const app = express();
-app.use('/graphql', express_graphql({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
+const apolloServer = new ApolloServer({typeDefs, resolvers})
+apolloServer.applyMiddleware({app})
 app.listen(process.env.PORT || 4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
